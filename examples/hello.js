8e58d7cfafba7004ee4e047e0724ee099c02c750
Palani Kumanan <palani.kumanan@dowjones.com>
2014-02-02 22:40:25 -0500

    Examples ...

---
+++ examples/hello.js
@@ -0,0 +1,28 @@
+var restpress = require('../index');
+var msg = new restpress('message');
+
+var data = [
+    {
+	"id" : 1,
+	"message" : "Hello, World!"
+    },
+    {
+	"id" : 2,
+	"message" : "Good Bye!"
+    }
+];
+
+msg.list(function(req, res) {
+	res.json(data);
+});
+
+msg.get(function(req, res) {
+	res.json(data[req.params.id - 1]);
+});
+
+var express = require('express');
+var app = express();
+
+msg.app(app);
+
+app.listen(3010);
