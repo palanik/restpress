9f2bb9c5755eee38aa770dca05ddb78dd314e91e
palanik <palani.kumanan@gmail.com>
2014-07-19 12:09:38 -0400

    Test case for action.json file

--- test/test.stars.js
+++ test/test.stars.js
@@ -117,3 +117,61 @@ describe('postApp-stars', function() {
 	});
 	
 });
+
+describe('fileAction-stars', function() {
+		var rp = new restpress('star', './test/star-actions.json');
+
+		// Methods
+		// List			
+		rp.list(function (req, res) {
+			res.send(200, "list stars");
+		});
+		
+		// Read
+		rp.read(function (req, res) {
+			res.send(200, "read star");
+		});
+		
+		// Create
+		rp.create(function (req, res) {
+			res.send(200, "create star");
+		});
+		
+		// Update
+		rp.update(function (req, res) {
+			res.send(200, "update star");
+		});
+		
+		// Delete
+		rp.delete(function (req, res) {
+			res.send(200, "delete star");
+		});
+		
+		var app = express();
+		app.use(express.bodyParser());
+		
+		var basePath = '/';
+		rp.app(app);
+		
+	it('Index', function(done) {
+		request(app).get(basePath + 'star').expect(200, "list stars", done);	
+	});
+
+	it('Create', function(done) {
+		request(app).post(basePath + 'star').expect(200, 'create star', done);		
+	});
+
+	it('Read', function(done) {
+		request(app).get(basePath + 'star/23').expect('read star', done);		
+	});
+
+	it('Update', function(done) {
+		request(app).put(basePath + 'star/23').expect(200, 'update star', done);		
+	});
+
+	it('Delete', function(done) {
+		request(app).del(basePath + 'star/23').expect(200, 'delete star', done);		
+	});
+	
+});
+
